d2c801259717507602ca4e2722a6281f014c3861
Raphael Monte <rmonte.dev@gmail.com>
2025-04-09 14:10:01 -0300

    Refactor App.vue to use MainTitle and MainSocialLinks components; update index.html title

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+defineProps({
+    title: String,
+    subtitle: String,
+})
+
+</script>
+
+<template>
+    <h1 class="title">{{ title }}</h1>
+    <p class="subtitle">{{ subtitle }}</p>
+</template>
+
+<style scoped>
+.subtitle {
+    color: #888;
+}
+
+.title {
+    will-change: filter;
+    transition: filter 300ms;
+    filter: drop-shadow(0 0 1em #ffffff);
+}
+</style>
